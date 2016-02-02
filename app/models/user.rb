8ff68e0af45f197392c0d3bf97feb19678305559
njabswalsh <njwalsh@stanford.edu>
2016-02-02 14:28:36 -0800

    removing salted passwords

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,14 @@
 class User < ActiveRecord::Base
 	
 	def password_valid? password
-		digest = Digest::SHA256.hexdigest(password + self.salt.to_s)
+		digest = Digest::SHA256.hexdigest(password) # + self.salt.to_s
 		puts "DIGEST: " + digest
 		return digest == self.password_digest
 	end
 
 	def User.create first_name, last_name, login, password
 		salt = Random.new().rand()
-		password_digest = Digest::SHA256.hexdigest(password + salt.to_s)
+		password_digest = Digest::SHA256.hexdigest(password) # + salt.to_s
 		user = User.new(:first_name => first_name, :last_name => last_name, :login => login, :salt => salt, :password_digest => password_digest)
 		user.save
 	end
