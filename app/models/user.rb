85e0cba3fd9be2526b0d61c75d25eb6f272b178b
njabswalsh <njwalsh@stanford.edu>
2016-02-02 15:22:41 -0800

    It works!

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,15 +2,11 @@ class User < ActiveRecord::Base
 	
 	def password_valid? password
 		digest = Digest::SHA256.hexdigest(password + self.salt.to_i.to_s)
-		puts "SALT: " + self.salt.to_i.to_s
-		puts "DIGEST: " + digest
-		puts "PASSWORD DIGEST: " + self.password_digest
 		return digest == self.password_digest
 	end
 
 	def User.create first_name, last_name, login, password
 		salt = Random.new().rand(2147483647)
-		puts "NEWLY CREATED SALT: " + salt.to_s
 		password_digest = Digest::SHA256.hexdigest(password + salt.to_s)
 		user = User.new(:first_name => first_name, :last_name => last_name, :login => login, :salt => salt, :password_digest => password_digest)
 		user.save
