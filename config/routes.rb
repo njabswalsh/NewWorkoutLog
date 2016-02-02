9ff0b635f280f6ea0f01d45f5e5f6c6868c96bf8
njabswalsh <njwalsh@stanford.edu>
2016-02-02 15:26:13 -0800

    updated app

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
+  
+  resources :posts
 
   # You can have the root of your site routed with "root"
   root 'application#homepage'
@@ -17,8 +19,6 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
-
-  resources :posts
 
   # Example resource route with options:
   #   resources :products do
