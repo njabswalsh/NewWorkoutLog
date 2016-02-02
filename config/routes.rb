7352cf002a1be4ef87ea32ebce03a3e6264404d0
njabswalsh <njwalsh@stanford.edu>
2016-02-01 23:49:20 -0800

    Changing routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'application#homepage'
 
+  # Temporary routing structure? (Nolan)
+  get ':controller(/:action(/:id))'
+  post ':controller(/:action(/:id))'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
@@ -13,6 +17,8 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+
+  resources :posts
 
   # Example resource route with options:
   #   resources :products do
