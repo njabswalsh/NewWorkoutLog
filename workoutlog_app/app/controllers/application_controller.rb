6f2d685af07b860c95793ad32c48860ee9c65c9b
njabswalsh <njwalsh@stanford.edu>
2016-02-06 12:43:06 -0800

    deleting workoutlog_app folder

--- workoutlog_app/app/controllers/application_controller.rb
+++
@@ -1,9 +0,0 @@
-class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
-
-  def homepage
-  	redirect_to("/users/home")
-  end
-end
