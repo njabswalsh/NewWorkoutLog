0521245d0bb4625f1647eac947f705fa6b7bd0b9
njabswalsh <njwalsh@stanford.edu>
2016-02-06 11:10:14 -0800

    Changed method of password salting

--- workoutlog_app/app/models/user.rb
+++ workoutlog_app/app/models/user.rb
@@ -1,13 +1,13 @@
 class User < ActiveRecord::Base
 	
 	def password_valid? password
-		digest = Digest::SHA1.hexdigest(password + self.salt.to_s)
+		digest = Digest::SHA256.hexdigest(password + self.salt.to_i.to_s)
 		return digest == self.password_digest
 	end
 
 	def User.create first_name, last_name, login, password
-		salt = Random.new().rand()
-		password_digest = Digest::SHA1.hexdigest(password + salt.to_s)
+		salt = Random.new().rand(2147483647)
+		password_digest = Digest::SHA256.hexdigest(password + salt.to_s)
 		user = User.new(:first_name => first_name, :last_name => last_name, :login => login, :salt => salt, :password_digest => password_digest)
 		user.save
 	end
