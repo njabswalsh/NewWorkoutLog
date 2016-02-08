40c96cd3c08972e9c626736f1b56cb44f018c2ac
Monisha White <mewhite@stanford.edu>
2016-02-07 22:35:43 -0800

    prettifying different views, arrow links added (but don't actually work)

--- db/migrate/20160202071844_create_posts.rb
+++ db/migrate/20160202071844_create_posts.rb
@@ -2,7 +2,7 @@ class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
       t.integer	 :user_id
-      t.datetime :date #TODO: should be .datetime
+      t.date     :date
       t.text 	 :workout
       t.text 	 :notes
 
