38a0491fa2d354cfee64443dda75cf78cada06b2
njabswalsh <njwalsh@stanford.edu>
2016-02-20 15:48:25 -0800

    Fixing lots of smaller todos. Username validation, not-case sensitive, date display etc.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
 	has_many :posts
 	has_and_belongs_to_many :teams
+	validates :login, presence: true
 
 	def password_valid? password
 		digest = Digest::SHA256.hexdigest(password + self.salt.to_i.to_s)
@@ -8,9 +9,12 @@ class User < ActiveRecord::Base
 	end
 
 	def User.create first_name, last_name, login, password
+		if password == ""
+			return false
+		end
 		salt = Random.new().rand(2147483647)
 		password_digest = Digest::SHA256.hexdigest(password + salt.to_s)
-		user = User.new(:first_name => first_name, :last_name => last_name, :login => login, :salt => salt, :password_digest => password_digest)
+		user = User.new(:first_name => first_name, :last_name => last_name, :login => login.downcase, :salt => salt, :password_digest => password_digest)
 		user.save
 	end
 
