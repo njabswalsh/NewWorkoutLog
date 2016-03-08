c1b47180d9ed7550d379b11639388cf506b05249
njabswalsh <njwalsh@stanford.edu>
2016-03-07 22:42:56 -0800

    Allowing users to enter nothing for sets and reps

--- app/helpers/exercises_helper.rb
+++ app/helpers/exercises_helper.rb
@@ -1,6 +1,15 @@
 module ExercisesHelper
 	def get_exercise_str(exercise)
-		str = exercise.exercise_name + ": " + exercise.sets.to_s + " x " + exercise.reps.to_s
+		str = exercise.exercise_name + ": "
+		if exercise.sets
+			str += exercise.sets.to_s
+			if exercise.reps
+				str += " x "
+			end
+		end
+		if exercise.reps
+			str += exercise.reps.to_s
+		end
 		if exercise.weight
 			str += " x " + exercise.weight.to_s
 		end
