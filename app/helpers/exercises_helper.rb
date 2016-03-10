104edb267b3b8267e09c0d08c7a8e0a12effafd6
Monisha White <mewhite@stanford.edu>
2016-03-09 22:59:02 -0800

    now display each exercise name only once, with a comma separated list of each setsxrepsxweight entry - in the team view - about to add for personal view also.

--- app/helpers/exercises_helper.rb
+++ app/helpers/exercises_helper.rb
@@ -1,17 +1,23 @@
 module ExercisesHelper
 	def get_exercise_str(exercise)
 		str = exercise.exercise_name + ": "
+		str += get_exercise_sets_reps_weight_string(exercise)
+		return str
+	end
+
+	def get_exercise_sets_reps_weight_string(exercise)
+		str = ""
 		if exercise.sets
 			str += exercise.sets.to_s
 			if exercise.reps
-				str += " x "
+				str += "x"
 			end
 		end
 		if exercise.reps
 			str += exercise.reps.to_s
 		end
 		if exercise.weight
-			str += " x " + exercise.weight.to_s
+			str += "x" + exercise.weight.to_s
 		end
 		return str
 	end
