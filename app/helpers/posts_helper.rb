1c982c76392b38ddf9bf6d04640e91758baf339f
Monisha White <mewhite@stanford.edu>
2016-03-10 00:06:57 -0800

    bullets, displaying exercises well in edit, team view, and my workouts index

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -3,16 +3,12 @@ module PostsHelper
 	def get_post_exercises_mapping(post)
 		exercises_hash = Hash.new
 		post.exercises.each do |exercise|
-			puts "EXERCISE NAME: " + exercise.exercise_name
 			entries = exercises_hash[exercise.exercise_name]
-			puts "ENTRIES BEFORE: " + entries.to_s
 			if entries
 				entries.push(exercise)
 			else
 				entries = [exercise]
 			end
-			puts "ENTRIES AFTER: " + entries.to_s
-
 			exercises_hash[exercise.exercise_name] = entries
 		end
 		return exercises_hash
